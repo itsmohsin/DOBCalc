1d2acf499920770e4146ad251602062091658a3d
itsmohsin <mohsinsmannat@gmail.com>
2022-11-29 09:58:31 +0530

    Added UI to App

--- app/src/main/java/io/github/itsmohsin/dobcalc/MainActivity.kt
+++ app/src/main/java/io/github/itsmohsin/dobcalc/MainActivity.kt
@@ -2,10 +2,18 @@ package io.github.itsmohsin.dobcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDate)
+
+        btnDatePicker.setOnClickListener {
+            Toast.makeText(this, "btnDatePicker Pressed", Toast.LENGTH_LONG).show()
+        }
     }
 }
